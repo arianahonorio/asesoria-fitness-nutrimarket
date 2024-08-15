47f652f96bddba20a8bcb051d96ace296b7da075
Ariana Honorio <hhariana@hotmail.com>
2024-08-15 12:21:40 -0300

    Avances2

--- js/main.js
+++ js/main.js
@@ -1,10 +1,12 @@
 //INICIO
+
 let bienvenida= document.getElementById("bienvenida")
 let banner= document.getElementById("banner")
 let text= document.getElementById("text")
 
 //FORMULARIO
 //nombre
+
 let inputNombre= document.getElementById("nombre")
 
 function retornarNombre () {
@@ -19,29 +21,26 @@ retornarNombre ()
 
 let inputEdad= document.getElementById("edad")
 
-
 //altura
+
 let inputAltura= document.getElementById("altura")
+
 inputAltura.addEventListener('input', () => { 
     inputAltura.value;
     localStorage.setItem("inputAltura", JSON.stringify(inputAltura))
 })
 
 //peso
-// agregar retorno de info en respuestas.js con fecha hasta el dia que completo la info con libreria luxon
+
 let inputPeso= document.getElementById("peso")
 inputPeso.addEventListener('input', () => { 
     inputPeso.value;
     localStorage.setItem("inputPeso", JSON.stringify(inputPeso))
 })
 
-
-
 //asignar nutricionista 
 let inputAlimentacion= document.getElementById("alimentacion")
 
-//esta funcion tendria que estar en respuestas.js? 
-// y crear elemento que retorne la info al usuario
 function retornarNutricionista (){
     inputAlimentacion.onchange = () => {
         const mostrarAlimentacion = alimentacion.filter(alimentacion => alimentacion.nutricionista === 
@@ -53,379 +52,49 @@ retornarNutricionista(alimentacion)
 
 //dietas
 //Plan Nutricionista de dieta Vegan-Federico 
-
-//pasar a json 
-const dietaVegan= [{
-    comida: 'desayuno',
-    receta: 'chocolatada de leche vegetal, tostadas con palta y tomate, fruta',
-},
-{
-    comida:'desayuno',
-    receta:'cafe, yogur de soja con avena, fruta',
-},
-{
-    comida: 'desayuno',
-    receta: 'batido de frambuesa con leche vegetal, pan integral con tomate y semillas, galleta de avena y pasas'
-},
-{
-    comida:'almuerzo',
-    receta:'wrap de ensalada de garbanzos y palta',
-},
-{
-    comida:'almuerzo',
-    receta:'sopa de albahaca y tomates asados',
-},
-{
-    comida:'almuerzo',
-    receta:'quinoa con verduras, ensalada de garbanzos',
-},
-{
-    comida:'cena',
-    receta:'pasta con hummus cremoso y vegetales',
-},
-{
-    comida:'cena',
-    receta:'wok de verduras, albondigas de berenjena',
-},
-{
-    comida:'cena',
-    receta:'tortilla rellena de espinaca, sopa de verduras',
-},
-]
-
 let buttonVegan= document.getElementById("vegan")
 
-//esta funcion tendria que estar en respuestas.js? 
-// y crear elemento que retorne la info al usuario
-buttonVegan.click = () => {
-    const mostrarDietaVegan = dietaVegan.forEach (dietaVegan => {(`Tu comida para ${dietaVegan.comida} es ${dietaVegan.receta}.`)},   
+buttonVegan.onclick = () => {
+    const mostrarDietaVegan = dietaVegan.find (dietaVegan => dietaVegan==clicks++,   
         localStorage.setItem("buttonVegan", JSON.stringify(buttonVegan)),
 )}
 
 //Plan Nutricionista de dieta Veggie-Andrea
-
-//pasar a json 
-const dietaVeggie= [{
-    comida: 'desayuno',
-    receta: 'cafe con bebida vegetal, tostada de pan integral, huevos revueltos, tomate troceado',
-},
-{
-    comida:'desayuno',
-    receta:'batido de banana con leche vegetal, tortitas de avena, nueces y chocolate',
-},
-{
-    comida: 'desayuno',
-    receta: 'yogur deslactosado con avena y frutos secos, fruta',
-},
-{
-    comida:'almuerzo',
-    receta:'mini tartas de calabacín y risotto con champiñones',
-    postre: 'fruta de estación',
-},
-{
-    comida:'almuerzo',
-    receta:'ensalada de tomates cherry, pimiento rojo y verde, cebolla y quinoa',
-    postre: 'fruta de estación',
-},
-{
-    comida:'almuerzo',
-    receta:'tallarines integrales, dados de tofu, con salteado de brócoli, zanahoria, berenjena, cebolla y semillas de sésamo',
-    postre: 'frutas de estación',
-},
-{
-    comida:'cena',
-    receta:'ensalada verde, tortilla de patatas',
-    postre: 'fruta de estación',
-},
-{
-    comida:'cena',
-    receta:'ensalada de tomate, cebolla, pepino, aguacate, maíz y mozzarella, caldo de verduras con fideos',
-    postre: 'fruta de estación',
-},
-{
-    comida:'cena',
-    receta:'Pizza de coliflor',
-    postre: 'fruta de estación',
-},
-]
-
-//esta funcion tendria que estar en respuestas.js? 
-// y crear elemento que retorne la info al usuario
 let buttonVeggie= document.getElementById("veggie")
-buttonVeggie.click = () => {
-    const mostrarDietaVeggie = dietaVeggie.forEach (mostrarDietaVeggie => {(`Tu comida para ${dietaVeggie.comida} es ${dietaVeggie.receta}.`)},   
+
+buttonVeggie.onclick = () => {
+    const mostrarDietaVeggie = dietaVeggie.find (dietaVeggie => dietaVeggie==clicks++,   
         localStorage.setItem("buttonVeggie", JSON.stringify(buttonVeggie)),
 )}
 
 //Plan Nutricionista de dieta Completa-Lucia
-
-//pasar a json
-const dietaCompleta= [
-{
-    comida: 'desayuno',
-    receta: 'cafe, tostadas con huevo revuelto, palta y tomate, porción de fruta',
-},
-{
-    comida:'desayuno',
-    receta:'tortitas de avena, porción de frutos secos',
-},
-{
-    comida: 'desayuno',
-    receta: 'insufión a elección, wok de yogur con avena, frutos secos y fruta',
-},
-{
-    comida:'almuerzo',
-    receta:'pollo a la plancha, arroz y verduras',
-    postre: 'flan',
-},
-{
-    comida:'almuerzo',
-    receta:'ensalada de cuscus con manzana y pollo',
-    postre: 'fruta de estación',
-},
-{
-    comida:'almuerzo',
-    receta:'albondigas de soja, quinoa con verduras',
-    postre: 'frutas de estación',
-},
-{
-    comida:'cena',
-    receta:'salteado de verdura, ternera',
-    postre:'fruta de estación',
-},
-{
-    comida:'cena',
-    receta:'tortilla de espinaca con cebolla y queso',
-    postre:'fruta de estación',
-},
-{
-    comida:'cena',
-    receta:'pasta con verduras, pollo',
-    postre:'fruta de estación',
-},
-]
-
 let buttonCompleta= document.getElementById("completa")
 
-//esta funcion tendria que estar en respuestas.js? 
-// y crear elemento que retorne la info al usuario
-buttonCompleta.click = () => {
-    const mostrarDietaCompleta = dietaCompleta.forEach (dietaCompleta => {(`Tu comida para ${dietaCompleta.comida} es ${dietaCompleta.receta}.`)},   
+buttonCompleta.onclick = () => {
+    const mostrarDietaCompleta = dietaCompleta.find (dietaCompleta => dietaCompleta==clicks++,   
         localStorage.setItem("buttonCompleta", JSON.stringify(buttonCompleta)),
 )}
 
 //Plan de entrenamineto-Pedro/Viviana/Ayelen
 //Rutina de enfasis a un grupo muscular
-
-//pasar a json
-const rutinaEnfasis=[
-{
-    dia:'lunes',
-    ejercicio:'press banca',
-    series:'4',
-    repeticiones:'6',
-}, {
-    dia:'lunes',
-    ejercicio:'aperturas inclinadas',
-    series: '4',
-    repeticiones: '10, 8, 8, 6',
-}, {
-    dia:'lunes',
-    ejercicio:'sentadilla profunda',
-    series: '3',
-    repeticiones: '10, 8, 6',
-},{
-    dia:'lunes',
-    ejercicio:'dominadas lastradas',
-    series: '4',
-    repeticiones: '10, 8, 8, 6',
-},{
-    dia:'lunes',
-    ejercicio:'press militar barra',
-    series: '3',
-    repeticiones: '10, 8, 8',
-},{
-    dia:'miercoles',
-    ejercicio:'sentadillas en cajón',
-    series: '4',
-    repeticiones: '10, 10, 8, 8',
-},{
-    dia:'miercoles',
-    ejercicio:'prensa 45°',
-    series: '4',
-    repeticiones: '8, 8, 6, 6',
-},{
-    dia:'miercoles',
-    ejercicio:'press banca inclinado con mancuernas',
-    series: '3',
-    repeticiones: '8, 6, 6',
-},{
-    dia:'miercoles',
-    ejercicio:'remo gironda',
-    series: '4',
-    repeticiones: '10, 8, 8, 6',
-},{
-    dia:'miercoles',
-    ejercicio:'elevaciones frontales',
-    series: '3',
-    repeticiones: '10, 10, 8',
-},{
-    dia:'miercoles',
-    ejercicio:'elevaciones de talones de pie',
-    series: '5',
-    repeticiones: '20',
-},{
-    dia:'viernes',
-    ejercicio:'dominadas neutras lastradas',
-    series: '4',
-    repeticiones: '6',
-},{
-    dia:'viernes',
-    ejercicio:'remo en punta',
-    series: '4',
-    repeticiones: '10, 8, 8, 6',
-},{
-    dia:'viernes',
-    ejercicio:'extensiones de cuadriceps',
-    series: '4',
-    repeticiones: '10, 10, 8, 6',
-},{
-    dia:'viernes',
-    ejercicio:'curl femoral',
-    series: '4',
-    repeticiones: '10, 10, 8, 8',
-},{
-    dia:'viernes',
-    ejercicio:'flexiones de suelo',
-    series: '4',
-    repeticiones: 'hasta el fallo',
-},{
-    dia:'viernes',
-    ejercicio:'fondos paralelas',
-    series: '4',
-    repeticiones: 'hasta el fallo',
-},{
-    dia:'viernes',
-    ejercicio:'encogimientos dobles',
-    series: '4',
-    repeticiones: '20',
-},{
-    dia:'viernes',
-    ejercicio:'elevacion de piernas',
-    series: '4',
-    repeticiones: '20',
-},
-]
-
 let buttonEnfasis= document.getElementById("enfasis")
 
-//esta funcion tendria que estar en respuestas.js? 
-// y crear elemento que retorne la info al usuario
-buttonEnfasis.click = () => {
-    const mostrarRutinaEnfasis = rutinaEnfasis.forEach (rutinaEnfasis => {(`El día ${rutinaEnfasis.dia} realiza ${rutinaEnfasis.ejercicio} de ${rutinaEnfasis.series} y ${rutinaEnfasis.repetiones}.`)},   
+buttonEnfasis.onclick = () => {
+    const mostrarRutinaEnfasis = rutinaEnfasis.find (rutinaEnfasis => rutinaEnfasis==clicks++ ,   
         localStorage.setItem("buttonEnfasis", JSON.stringify(buttonEnfasis)),
 )}
 
 //Rutina Clasica
-
-//pasar a json
-const rutinaClasica= [
-{
-    dia:'lunes-miercoles',
-    ejercicio:'sentadillas',
-    series: '3',
-    repeticiones: '12',
-},{
-    dia:'lunes-miercoles',
-    ejercicio:'remo con barra con barra/mancuernas',
-    series: '3',
-    repeticiones: '12',
-},{
-    dia:'lunes-miercoles',
-    ejercicio:'press militar con barra/mancuernas',
-    series: '3',
-    repeticiones: '12',
-},{
-    dia:'lunes-miercoles',
-    ejercicio:'press de banca',
-    series: '3',
-    repeticiones: '12',
-},{
-    dia:'lunes-miercoles',
-    ejercicio:'curl de biceps',
-    series: '3',
-    repeticiones: '12',
-},{
-    dia:'lunes-miercoles',
-    ejercicio:'fondos paralelas',
-    series: '5',
-    repeticiones: '10',
-},{
-    dia:'lunes-miercoles',
-    ejercicio:'elevaciones de talones con barra',
-    series: '3',
-    repeticiones: '20',
-},{
-    dia:'lunes-miercoles',
-    ejercicio:'abdominales oblicuos y cortos',
-    series: '8',
-    repeticiones: '20',
-},{
-    dia:'viernes',
-    ejercicio:'sentadillas',
-    series: '3',
-    repeticiones: '12',
-},{
-    dia:'viernes',
-    ejercicio:'peso muerto',
-    series: '3',
-    repeticiones: '12',
-},{
-    dia:'viernes',
-    ejercicio:'press militar con barra/mancuernas',
-    series: '3',
-    repeticiones: '12',
-},{
-    dia:'viernes',
-    ejercicio:'press de banca con barra/mancuernas',
-    series: '3',
-    repeticiones: '12',
-},{
-    dia:'viernes',
-    ejercicio:'curl de biceps con barra/mancuernas',
-    series: '3',
-    repeticiones: '12',
-},{
-    dia:'viernes',
-    ejercicio:'fondos paralelas',
-    series: '5',
-    repeticiones: '10',
-},{
-    dia:'viernes',
-    ejercicio:'elevaciones de talones con barra',
-    series: '3',
-    repeticiones: '20',
-},{
-    dia:'viernes',
-    ejercicio:'abdominales oblicuos y cortos',
-    series: '8',
-    repeticiones: '20',
-},
-]
-
 let buttonClasica= document.getElementById("clasica")
 
-//esta funcion tendria que estar en respuestas.js? 
-// y crear elemento que retorne la info al usuario
-buttonClasica.click = () => {
-    const mostrarRutinaClasica = rutinaClasica.forEach (rutinaClasica => {(`El día ${rutinaClasica.dia} realiza ${rutinaClasica.ejercicio} de ${rutinaClasica.series} series y ${rutinaClasica.repetiones} repeticiones.`)},   
+buttonClasica.onclick = () => {
+    const mostrarRutinaClasica = rutinaClasica.forEach (rutinaClasica => rutinaClasica==clicks++,   
         localStorage.setItem("buttonClasica", JSON.stringify(buttonClasica)),
 )}
 
 //Rutina avanzada
 //Todos los días_Cuerpo completo
 
-//pasar a json
 const rutinaAvanzada=[
 {
     dia:'todos-los-dias',
@@ -504,7 +173,7 @@ let buttonAvanzada= document.getElementById("avanzada")
 
 //esta funcion tendria que estar en respuestas.js? 
 // y crear elemento que retorne la info al usuario
-buttonAvanzada.click = () => {
+buttonAvanzada.onclick = () => {
     const mostrarRutinaAvanzada = rutinaAvanzada.forEach (rutinaAvanzada => {(`El día ${rutinaAvanzada.dia} realiza ${rutinaAvanzada.ejercicio} de ${rutinaAvanzada.series} series y ${rutinaAvanzada.repetiones} repeticiones.`)},   
         localStorage.setItem("buttonAvanzada", JSON.stringify(buttonAvanzada)),
 )}
@@ -522,7 +191,7 @@ inputEmail.addEventListener('input', () => {
 
 //button-enviar
 let buttonGuardar = document.getElementById("guardar")
-buttonGuardar.click = () => {click++}
+buttonGuardar.onclick = () => {click++}
 localStorage.getItem(JSON.stringify(buttonGuardar))
 
 //formulario
--- js/respuestas.js
+++ js/respuestas.js
@@ -1,4 +1,6 @@
 //bienvenida
+let bienvenida= document.getElementById("bienvenida")
+
 let nombreStorage = localStorage.getItem("inputNombre")
 nombreStorage = JSON.parse(nombreStorage)
 
@@ -11,15 +13,79 @@ function retornarBienvenida (retornarNombre) {
 retornarBienvenida(nombreStorage)
 
 //peso inicial hasta la fecha con luxon
+let pesoALaFecha= document.getElementById("peso-inicial")
 
 //retornar nutricionista
+let nutricionista= document.getElementById("nutricionista")
 
+let nutricionistaStorage = localStorage.getItem("inputAlimentacion")
+nutricionistaStorage = JSON.parse(nutricionistaStorage)
 
+fetch("./db/alimetacion.json")
+.then(response => response.json())
+.then(data => {
+        data.filter(alimentacion => {
+        const cardAlimentacion = document.createElement("div")
+        cardAlimentacion.innerHTML = `<h3>Tu tipo de alimentación es <span>${alimentacion.tipo}</span>.</h3>
+                        <h3>Tu nutricionista recomendado es <span>${alimentacion.nutricionista}</span>.</h3>`
+            container.appendChild(cardAlimentacion)
+        })
+    })
 
+//dietas
+let comidas= document.getElementById("lista-comidas")
 
-//dieta
+//dieta-vegan
+let veganStorage = localStorage.getItem("vegan")
+veganStorage = JSON.parse(veganStorage)
+
+fetch("./db/dieta-vegan.json")
+.then(response => response.json())
+.then(data => {
+        data.forEach(dietaVegan => {
+        const cardVegan = document.createElement("div")
+        cardVegan.innerHTML = `<h2>Dieta a seguir: </h2>
+                            <h3>Comida: ${dietaVegan.comida}</h3>
+                            <h3>Receta: ${dietaVegan.receta}</h3>`
+        container.appendChild(cardVegan)
+        })
+    })
+
+//dieta-veggie
+let veggieStorage = localStorage.getItem("veggie")
+veggieStorage = JSON.parse(veggieStorage)
+
+fetch("./db/dieta-veggie.json")
+.then(response => response.json())
+.then(data => {
+        data.forEach(dietaVeggie => {
+        const cardVeggie = document.createElement("div")
+        cardVeggie.innerHTML = `<h2>Dieta a seguir: </h2>
+                        <h3>Comida: ${dietaVeggie.comida}</h3>
+                        <h3>Receta: ${dietaVeggie.receta}</h3>`
+        container.appendChild(cardVeggie)
+        })
+    })
+
+//dieta-completa
+let completaStorage = localStorage.getItem("completa")
+completaStorage = JSON.parse(completaStorage)
+
+fetch("./db/dieta-completa.json")
+.then(response => response.json())
+.then(data => {
+        data.forEach(dietaCompleta => {
+        const cardCompleta = document.createElement("div")
+        cardCompleta.innerHTML = `<h2>Dieta a seguir: </h2>
+                        <h3>Comida: ${dietaCompleta.comida}</h3>
+                        <h3>Receta: ${dietaCompleta.receta}</h3>`
+        container.appendChild(cardCompleta)
+        })
+    })
 
 //retornar entrenador
+let entrenador= document.getElementById("retorno-entrenador")
+
 let edadStorage = localStorage.getItem("inputEdad")
 edadStorageStorage = JSON.parse(edadStorage)
 
@@ -42,6 +108,43 @@ function retornarProfe () {
 retornarProfe()
 
 //rutina
+let rutinas= document.getElementById("lista-rutinas")
 
+//rutina-enfasis
+let rutinaEnfasisStorage = localStorage.getItem("enfasis")
+rutinaEnfasisStorage = JSON.parse(rutinaEnfasisStorage)
 
+fetch("./db/rutina-enfasis.json")
+.then(response => response.json())
+.then(data => {
+        data.forEach(rutinaEnfasis => {
+        const cardRutiEnfasis = document.createElement("div")
+        cardRutiEnfasis.innerHTML = `<h2>Tu rutina a seguir: </h2>
+                        <div><li><h3>Día: ${rutinaEnfasis.dia}</h3>
+                        <h3>Ejercicio: ${rutinaEnfasis.ejercicio}</h3>
+                        <h3>Hacer <span>${rutinaEnfasis.series}</span> de <span>${rutinaEnfasis.repeticiones}</span>.</h3>
+                        </li></div>`
+        container.appendChild(cardRutiEnfasis)
+        })
+    })
+
+//rutina-clasica
+let rutinaClasicaStorage = localStorage.getItem("clasica")
+rutinaClasicaStorage = JSON.parse(rutinaClasicaStorage)
+
+fetch("./db/rutina-clasica.json")
+.then(response => response.json())
+.then(data => {
+        data.forEach(rutinaClasica => {
+        const cardRutiClasica = document.createElement("div")
+        cardRutiClasica.innerHTML = `<h2>Tu rutina a seguir: </h2>
+                        <div><li><h3>Día: ${rutinaClasica.dia}</h3>
+                        <h3>Ejercicio: ${rutinaClasica.ejercicio}</h3>
+                        <h3>Hacer <span>${rutinaClasica.series}</span> de <span>${rutinaClasica.repeticiones}</span>.</h3>
+                        </li></div>`
+        container.appendChild(cardRutiClasica)
+        })
+    })
+
+//rutina-avanzada
 
